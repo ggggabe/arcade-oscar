9394faf0cf40a941917cd004bcee6055b4f92653
Zack <lambertzackt@gmail.com>
2020-11-17 18:38:50 -0500

    cleaning

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,7 +1,4 @@
-import {
-  Link,
-  BrowserRouter as Router,
-} from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
 export const Home = () => {
 
--- src/views/Rps.js
+++ src/views/Rps.js
@@ -1,18 +1,45 @@
 import React from 'react';
 
-const varTest = ''
+const showButtons = () => {
+  // something that changes the page state to disply interactive buttons that will pass the weapon choice to the program
+}
 
-const oscarScore = 69
-const myScore = 0
+const myContext = { value: 'A' };
 
-export const Rps = () => (
+myFunction.call(myContext);  // logs { value: 'A' }
+myFunction.apply(myContext); // logs { value: 'A' }
+
+// const compareWeapons = () => {
+//   let winner;
+//   console.log(oscarWeapon)
+//   (oscarWeapon = 'rock') ? winner = 'me' : winner = 'oscar' ;
+//   console.log(winner)
+// }
+
+const startGame = () => {
+  // const randy = Math.floor(Math.random()*10)
+  // const oscarWeapon = (randy > 5) ? 'rock' : 'boulder'
+  // compareWeapons(oscarWeapon)
+}
+
+export const Rps = () => {
+  const oscarScore = 42
+  const myScore = 3
+
+  return (
 <>
   <div className='scoreKeeperNav'>
-  <div className='oscarScore'>Oscar {oscarScore}</div>
-  <div className='myScore'>You {myScore}</div>
+    <div className='oscarScore'>Oscar {oscarScore}</div>
+    <div className='myScore'>You {myScore}</div>
   </div>
 
-  <h1> Rock Paper Scissors </h1>
-  <div className='startButton'> Start Game </div>
+  <div className='gameContainer'>
+    <h1 style={{whiteSpace:'noWrap'}}> Rock Paper Scissors </h1>
+    <div className='startButton' onClick={startGame}>
+      <h3>Start Game</h3>
+    </div>
+
+  </div>
 </>
 )
+}
