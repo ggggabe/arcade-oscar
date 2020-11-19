65400417785e7aca807052b242166e9e285cc90f
Zack <lambertzackt@gmail.com>
2020-11-18 22:29:48 -0500

    building rps

--- src/views/Rps.js
+++ src/views/Rps.js
@@ -1,43 +1,65 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const showButtons = () => {
-  // something that changes the page state to disply interactive buttons that will pass the weapon choice to the program
+const assignOscarWeapon = () => {
+  let oscarWeapon;
+  const randyNum = Math.floor(Math.random()*3)
+  if (randyNum === 1){
+    oscarWeapon = 'rock'
+  }
+  else if (randyNum === 2){
+    oscarWeapon = 'paper'
+  }
+  else {
+    oscarWeapon = 'scissors'
+  }
+  return oscarWeapon
 }
 
-const myContext = { value: 'A' };
-
-myFunction.call(myContext);  // logs { value: 'A' }
-myFunction.apply(myContext); // logs { value: 'A' }
-
-// const compareWeapons = () => {
-//   let winner;
-//   console.log(oscarWeapon)
-//   (oscarWeapon = 'rock') ? winner = 'me' : winner = 'oscar' ;
-//   console.log(winner)
-// }
-
 const startGame = () => {
-  // const randy = Math.floor(Math.random()*10)
-  // const oscarWeapon = (randy > 5) ? 'rock' : 'boulder'
-  // compareWeapons(oscarWeapon)
+  const oscarWeapon = assignOscarWeapon()
+  console.log(oscarWeapon)
+
 }
 
 export const Rps = () => {
-  const oscarScore = 42
-  const myScore = 3
+
+  const [started,setStart] = useState(false)
+  const [userWeapon,setWeapon] = useState('')
+
+  const oscarWeapon = assignOscarWeapon()
+  console.log(oscarWeapon , userWeapon)
+
+  switch (userWeapon) {
+    case 'rock':
+    oscarWeapon === 'paper' ? console.log('oscar wins, paper beats rock') : console.log('nada')
+    break;
+    case 'paper':
+    (oscarWeapon === 'scissors') ? console.log('oscar wins, scissors beat paper') : console.log('nada')
+    break;
+    case 'scissors':
+    (oscarWeapon === 'rock') ? console.log('oscar wins, rock beat scissors') : console.log('nada')
+    break;
+    default:
+    console.log('You win!')
+  }
 
   return (
 <>
-  <div className='scoreKeeperNav'>
-    <div className='oscarScore'>Oscar {oscarScore}</div>
-    <div className='myScore'>You {myScore}</div>
-  </div>
-
   <div className='gameContainer'>
     <h1 style={{whiteSpace:'noWrap'}}> Rock Paper Scissors </h1>
-    <div className='startButton' onClick={startGame}>
-      <h3>Start Game</h3>
-    </div>
+    {!started && <div className='bigButton' onClick={() => {
+      startGame()
+      setStart(true)
+    }}>
+      Start Game
+    </div>}
+    {started &&
+      <>
+      <div className='bigButton' onClick={() => {setWeapon('rock')}}> Rock </div>
+      <div className='bigButton' onClick={() => {setWeapon('paper')}}> Paper </div>
+      <div className='bigButton' onClick={() => {setWeapon('scissors')}}> Scissors </div>
+    </>
+  }
 
   </div>
 </>
